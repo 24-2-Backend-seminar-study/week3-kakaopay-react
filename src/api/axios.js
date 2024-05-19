16457352f318f6a257a9c4bb777aa1e56b76d2d3
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-19 15:22:25 +0900

    feat: create kakaopay instance

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { getCookie } from '../utils/cookie';
 import { refreshToken } from './api';
+import { KAKAO_PAY_KEY } from '../constant';
 
 axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.withCredentials = true;
@@ -8,6 +9,15 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
 
 export const instance = axios.create();
+export const kakaoPayInstance = axios.create(
+  {
+    baseURL: "https://openapi.kaopay.com/v1/online/payment",
+    headers: {
+      Authorization: `SECRET_KEY ${KAKAO_PAY_KEY}`,
+      "Content-Type": "application/json"
+    },
+  }
+);
 
 instanceWithToken.interceptors.request.use(
   (config) => {
---
+++ src/constant/index.js
@@ -0,0 +1,2 @@
+export const KAKAO_PAY_KEY = process.env.REACT_APP_PAY_KEY;
+export const CID = process.env.REACT_APP_CID;

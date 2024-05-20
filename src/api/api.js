4cec10699fc9199707767a033bf410f6a189efc7
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-20 15:45:59 +0900

    add import statement

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+import { instance, instanceWithToken } from "./axios";
+
 export const refreshToken = async (token) => {
   const response = await instance.post("/account/refresh/", { refresh: token });
   if (response.status === 200) {

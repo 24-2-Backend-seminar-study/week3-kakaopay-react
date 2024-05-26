cf95b6ebc030dc6633d2d2c882156eec79344570
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-26 17:14:40 +0900

    add approval, cancel, fail url settings

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,6 @@
 export const KAKAO_PAY_KEY = process.env.REACT_APP_PAY_KEY;
 export const CID = process.env.REACT_APP_CID;
+
+export const APPROVAL_URL = "http://localhost:3000/approval";
+export const CANCEL_URL = "http://localhost:3000/cancel";
+export const FAIL_URL = "http://localhost:3000/fail";

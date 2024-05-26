310e0a99879e456c200332c68d7f7d6413b59097
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-26 17:13:50 +0900

    feat: page route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './routes/HomePage';
 import PayPage from './routes/PayPage';
+import FailPage from './routes/FailPage';
+import CancelPage from './routes/CancelPage';
+import ApprovalPage from './routes/ApprovalPage';
+import PayHistoryPage from './routes/PayHistoryPage';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pay" element={<PayPage />} />
+        <Route path="/fail" element={<FailPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/approval" element={<ApprovalPage />} />
+        <Route path="/pay-history" element={<PayHistoryPage />} />
       </Routes>
       </BrowserRouter>
     </div>

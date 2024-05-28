f1c493011ed4212a4c33b4a1537683d307783002
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-28 11:31:26 +0900

    feat: pay approval, cancel, fail page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pay" element={<PayPage />} />
+        <Route path="/fail" element={<FailPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/approval" element={<ApprovalPage />} />
+        <Route path="/pay-history" element={<PayHistoryPage />} />
       </Routes>
       </BrowserRouter>
     </div>

de8da437974a5ad7bb3f728bdc0763f9d090ff63
Nam Hee KIM <nami200284@snu.ac.kr>
2024-05-28 22:12:14 +0900

    add imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './routes/HomePage';
 import PayPage from './routes/PayPage';
+import FailPage from './routes/FailPage';
+import CancelPage from './routes/CancelPage';
+import ApprovalPage from './routes/ApprovalPage';
+import PayHistoryPage from './routes/PayHistoryPage';
 
 function App() {
   return (
